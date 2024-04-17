e44acbc4a99eafee527560f37a73d30ed2a8fd49
Ibrahim <ikomna@hispwca.org>
2024-04-17 10:17:56 +0000

    feat: add api version in the home file

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import i18n from "@dhis2/d2-i18n"
+import i18n from '@dhis2/d2-i18n'
+import { useConfig } from '@dhis2/app-runtime'
+import { Tag } from '@dhis2/ui'
 
-export const Home = () => (
-    <div>
-        <h1>{i18n.t("Hello ibrahim")}</h1>
-        <p>DHIS2 Web App Academy 2024</p>
-    </div>
-)
+export const Home = () => {
+    const { apiVersion } = useConfig()
+
+    return (
+        <div>
+            <h1>{i18n.t('Hello ibrahim')}</h1>
+            <p>DHIS2 Web App Academy 2024</p>
+            <Tag positive>
+                {i18n.t('Running on  API Version:  {{version}}', { version: apiVersion })}
+            </Tag>
+        </div>
+    )
+}
